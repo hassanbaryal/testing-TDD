fbc0e640f11f128c849e58c29856db69aba5dcf4
hassanbaryal <h.baryal2@gmail.com>
2022-11-07 13:07:59 -0500

    Created tests for Calculator Object

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { capitalize, reverseString } from './index';
+import { Calculator, capitalize, reverseString } from './index';
 
 // CAPITAILIZE TESTS
 describe('capitalize', () => {
@@ -19,5 +19,30 @@ describe('reverseString', () => {
 
   test.only('Reverses String', () => {
     expect(reverseString('Ting tang.')).toMatch(/.gnat gniT/);
+  });
+});
+
+describe('Calculator Object', () => {
+  // const calculator = new Calculator();
+  test.only('add method', () => {
+    expect(Calculator.add(1, 2)).toBe(3);
+  });
+
+  test.only('subtract method', () => {
+    expect(Calculator.subtract(1, 3)).toBe(-2);
+  });
+
+  test.only('divide method: non-zero division', () => {
+    expect(Calculator.divide(4, -2)).toBe(-2);
+    expect(Calculator.divide(2, 4)).toBeCloseTo(0.5);
+  });
+
+  test.only('divide method: zero division', () => {
+    expect(Calculator.divide(4, 0)).toBe(Infinity);
+  });
+
+  test.only('multiply method', () => {
+    expect(Calculator.multiply(4, 3)).toBe(12);
+    expect(Calculator.multiply(4, 0.3)).toBeCloseTo(1.2);
   });
 });
