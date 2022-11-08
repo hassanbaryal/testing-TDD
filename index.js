0405eff090e8a60dafbecc14e70a8f4925bca5b9
hassanbaryal <h.baryal2@gmail.com>
2022-11-07 22:07:28 -0500

    Created caeserCipher functionn

--- index.js
+++ index.js
@@ -28,4 +28,26 @@ function reverseString(str) {
   return newStr.join('');
 }
 
-export { Calculator, capitalize, reverseString };
+const caesarCipher = (str, shift) => {
+  if (shift === 0) return str;
+  const newStr = str.split('');
+  for (let i = 0; i < str.length; i += 1) {
+    // use charCodeAt and fromCharCode
+    const currentCharCode = str.charCodeAt(i);
+    let newCharCode = currentCharCode + shift;
+    if (currentCharCode >= 65 && currentCharCode <= 90) {
+      if (newCharCode > 90) newCharCode -= 26;
+      if (newCharCode < 65) newCharCode += 26;
+      newStr[i] = String.fromCharCode(newCharCode);
+    } else if (currentCharCode >= 97 && currentCharCode <= 122) {
+      if (newCharCode > 122) newCharCode -= 26;
+      if (newCharCode < 97) newCharCode += 26;
+      newStr[i] = String.fromCharCode(newCharCode);
+    }
+  }
+  return newStr.join('');
+};
+
+export {
+  Calculator, capitalize, reverseString, caesarCipher,
+};
